01446f89809c052b1c82ce1aa11d23313b751675
Julia <yuliya94101@gmail.com>
2020-04-28 20:38:50 +0300

    second commit

--- src/main/java/com/company/Item.java
+++ src/main/java/com/company/Item.java
@@ -1,46 +1,4 @@
 package com.company;
 
-import javax.persistence.*;
-import java.io.Serializable;
-import javax.validation.constraints.NotNull;
-
-
-@Entity
-@Table(name="item")
-public class Item implements Serializable {
-    @Id
-    @GeneratedValue
-    private long id;
-
-    @Column(name="name")
-    @NotNull
-    private String name;
-
-    @Column(name="status")
-    @NotNull
-    private String status;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+public class Item {
 }
